100eb509617f5def3ba8457eaf0d5a6d26ca5028
Josue9102 <161972024+Josue9102@users.noreply.github.com>
2024-04-14 11:22:47 -0200

    Add files via upload

---
+++ wow(Copy).js
@@ -0,0 +1,16 @@
+(function (ext) {
+    // Extension code will go here
+})(ScratchExtensions);
+(function (ext) {
+    // Define the block
+    ext.in_all_sprites = function() {
+        // Code to execute in all sprites
+    };
+
+    // Register the block, along with its metadata
+    ScratchExtensions.register('In All Sprites', {
+        blocks: [
+            [' ', 'In All Sprites', 'in_all_sprites']
+        ]
+    });
+})(ScratchExtensions);
